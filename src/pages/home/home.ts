05197388b2b1ffe28c0ba1cbf3785bf8eade420c
Felipe Brandao <felipe.brandao97@outlook.com>
2021-02-08 16:37:28 -0300

    Lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+@IonicPage() // indica que esssa classe é uma página e ela poderá ser referenciada entre aspas
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
