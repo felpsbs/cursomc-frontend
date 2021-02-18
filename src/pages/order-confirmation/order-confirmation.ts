9100fa1eae83ee7e0f42847c8bb7cf68cda1ab6a
Felipe Brandao <felipe.brandao97@outlook.com>
2021-02-18 17:14:07 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
       
       this.pedido = this.navParams.get('pedido');
   }
@@ -41,13 +43,31 @@ export class OrderConfirmationPage {
     );
   }
 
-  private findEnderecos(id: string, enderecos: EnderecoDTO[]) : EnderecoDTO {
-    let position = enderecos.findIndex(endereco => endereco.id == id);
-    return enderecos[position];
+  checkout() {
+    this.pedidoService.insert(this.pedido).subscribe(
+      response => { 
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location')); 
+      },
+      error=> {
+        if(error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      }
+    );
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
   }
 
   total() {
     this.cartService.getTotal();
   }
 
+  private findEnderecos(id: string, enderecos: EnderecoDTO[]) : EnderecoDTO {
+    let position = enderecos.findIndex(endereco => endereco.id == id);
+    return enderecos[position];
+  }
+  
 }
